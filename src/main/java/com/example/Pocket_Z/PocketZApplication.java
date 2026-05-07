c1757d8eba540bdbac2958b8b7310e33c1b557f4
alexa <pa.rico@duocuc.cl>
2026-05-07 18:48:38 -0400

    Inicio proyecto

---
+++ src/main/java/com/example/Pocket_Z/PocketZApplication.java
@@ -0,0 +1,13 @@
+package com.example.Pocket_Z;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocketZApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocketZApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Pocket_Z/PocketZApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Pocket_Z;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocketZApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
